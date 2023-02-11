e97cd9007d13fd7caece93f58d0474156885669c
sguiry40 <sguiry40@gmail.com>
2023-02-10 23:53:17 -0500

    bcm

--- src/motion.py
+++ src/motion.py
@@ -11,7 +11,7 @@ right2 = 35
 enb = 37
 
 # attach pins to gpio
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(left1, GPIO.OUT)
 GPIO.setup(left2, GPIO.OUT)
 GPIO.setup(right1, GPIO.OUT)
