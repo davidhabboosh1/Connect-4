a3e8a92a703d7b80908d085bb4e7968b2dc34701
gavin-white <gavin.white0001@gmail.com>
2023-02-10 22:05:23 -0500

    add src files

---
+++ src/montecarlosolver.py
@@ -0,0 +1,25 @@
+
+
+class Connect4Solver():
+    player = 1
+
+    def __init__(self):
+        self.board = [[0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0],]
+        pass
+
+    def updateState(self, board):
+        self.board = board
+        pass
+
+    def nextMove(self):
+        pass
+
+    def _solve(self):
+        pass
+    
+    
